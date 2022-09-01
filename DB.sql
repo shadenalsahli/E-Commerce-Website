6c7f860867651494fd72b540a4f7be27388450c1
Shaden <58245598+shadenalsahli@users.noreply.github.com>
2022-09-02 02:49:29 +0300

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,25 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Sep 01, 2022 at 10:33 PM
--- Server version: 10.5.16-MariaDB-cll-lve
--- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `u632986454_DB`
---
 
 -- --------------------------------------------------------
 
